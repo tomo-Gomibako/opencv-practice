55df971e67276ef6f54baf72e7136ea1638b3a3c
tomon9086 <tomo.mouse.9086@gmail.com>
2019-12-23 13:54:16 +0900

    :shower:remove comment

--- src/main.py
+++ src/main.py
@@ -6,9 +6,3 @@ from gui import Window
 lenna = cv2.imread(str(Path("Lenna.png").resolve()))
 
 Window(lenna).run()
-
-# cv2.startWindowThread()
-# cv2.namedWindow("image")
-# cv2.imshow("image", lenna)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
